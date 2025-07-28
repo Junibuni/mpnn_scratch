21c2eed80bd0de73a6cdf9e7fd2803579e97b78b
Junibuni <rich4103@naver.com>
2025-07-28 12:14:02 +0900

    add train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+
+def train(model, loader, optimizer, device):
+    model.train()
+    total_loss = 0
+    for data in loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.cross_entropy(out, data.y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
