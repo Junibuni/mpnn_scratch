eeca6c707c8b69a46d4bd3328cf8cae5e9837ecd
Junibuni <rich4103@naver.com>
2025-07-28 12:14:21 +0900

    add test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import torch
+
+@torch.no_grad()
+def test(model, loader, device):
+    model.eval()
+    correct = 0
+    for data in loader:
+        data = data.to(device)
+        out = model(data)
+        pred = out.argmax(dim=1)
+        correct += int((pred == data.y).sum())
+    return correct / len(loader.dataset)
